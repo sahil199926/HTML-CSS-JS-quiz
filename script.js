234b03644479104a4e3b022768ba95fc1481f7e1
sahil jaryal <60871687+sahil199926@users.noreply.github.com>
2021-03-09 23:19:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function func(){
  document.getElementById('index').style.display='none';
  document.getElementById('q-1').style.display='block';
@@ -32,10 +31,51 @@ function go(event,a){
 function start(){
     document.getElementById('result').style.display='none';
     document.getElementById('index').style.display='block';
+
+   
 }
 
 function sub(event){
     var x=event.target.parentElement.parentElement.parentElement;
     x.style.display='none';
     x.nextElementSibling.style.display='block';
+    
+
 }
+var m=9;
+var s=59;
+function min() {
+    m-=1;
+    document.getElementById("timer").innerHTML=m+":"+s;
+    
+}
+function sec() {
+    if(s===0){s=59;}
+    else{s-=1;}
+    document.getElementById("timer").innerHTML=m+":"+s;}
+
+
+var a=true;
+var mm;
+var ss;
+function time(){
+    if(a){
+       mm= setInterval(min, 60000);
+
+       ss= setInterval(sec, 1000);
+        
+        a=false;
+    document.getElementById('timer').style.display='block';
+    }
+    else{
+        a=true;
+    document.getElementById('timer').innerHTML=" ";
+    document.getElementById('timer').style.display='none';
+    clearInterval(mm);
+    clearInterval(ss);
+    m=9;
+    s=59;
+    }
+       
+}
+
