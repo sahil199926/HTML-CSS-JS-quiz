5f6cb2362b967331bee5957e23cac578f9b93f78
sahil jaryal <60871687+sahil199926@users.noreply.github.com>
2021-03-12 19:18:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function func(){
  document.getElementById('index').style.display='none';
  document.getElementById('q-1').style.display='block';
 }
-
+//go privious
 function prev(event){
 
     var a= event.target.parentElement.parentElement.parentElement.parentElement.parentElement;
@@ -10,14 +10,14 @@ function prev(event){
     a.previousElementSibling.style.display='block';
     
 }
-
+//go next
 function next(event){
 
     var b= event.target.parentElement.parentElement.parentElement.parentElement.parentElement;
     b.style.display='none';
     b.nextElementSibling.style.display='block';
 }
-
+//footer to move qouckly to  any visitted page
 function go(event,a){
     var b= event.target.parentElement.parentElement.parentElement.parentElement.parentElement;
     b.style.display='none';
@@ -28,36 +28,91 @@ function go(event,a){
     }
     c.style.display='block';
 }
+//start again
 function start(){
     document.getElementById('result').style.display='none';
     document.getElementById('index').style.display='block';
-
+    document.getElementById('sol').style.display='none';
+    location.reload();
    
 }
-
+ans={'q1':0,'q2':0,'q3':0,'q4':0,'q5':0,'q6':0,'q7':0,'q8':0,'q9':0,'q10':0,};
+solution={'q1':'0','q2':'0','q3':'0','q4':'0','q5':'0','q6':'0','q7':'0','q8':'0','q9':'0','q10':'0'};
+// for answer submition
 function sub(event){
+    var cal=event.target.parentElement.previousElementSibling;
+    
+    for(i=0;i<cal.childNodes.length;i++){
+        
+        if(cal.childNodes[i].checked&&cal.childNodes[i].value==='y')
+        { solution[cal.childNodes[i].name]=cal.childNodes[i].id; ans[cal.childNodes[i].name]=1;}
+
+        else if(cal.childNodes[i].checked){solution[cal.childNodes[i].name]=cal.childNodes[i].id;ans[cal.childNodes[i].name]=2;}
+       
+    }
+
     var x=event.target.parentElement.parentElement.parentElement;
+    if(x.id==='q-10'){return;}
     x.style.display='none';
     x.nextElementSibling.style.display='block';
     
 
 }
+
+//result
+function result(){
+    document.getElementById('q-10').style.display='none';
+    document.getElementById('result').style.display='block';
+    var r=0;
+    for(let p in ans){
+        if(ans[p]==1){r+=1;}
+    }
+    document.getElementById("res").innerHTML='You have scored '+r;
+}
+
+function sure(c){
+    if(c=='c'){document.getElementsByClassName('modal')[0].style.display='none';}
+    else{document.getElementsByClassName('modal')[0].style.display='block';}
+
+}
+var mm;
+var ss;
 var m=9;
 var s=59;
+//minite fun
 function min() {
-    m-=1;
+    if(m===-1){
+        alert('TIME IS UP');
+        m=9;
+        s=59;
+        result();
+        clearInterval(mm);
+        clearInterval(ss);
+
+        var c = document.getElementById('index');
+
+        for (i = 0; i < 11; i++) {
+            c.style.display = 'none';
+            c = c.nextElementSibling;
+        }
+        time();
+        c.style.display = 'block';
+    }
+
+    else{m-=1;}
     document.getElementById("timer").innerHTML=m+":"+s;
     
 }
+//second func
 function sec() {
     if(s===0){s=59;}
     else{s-=1;}
-    document.getElementById("timer").innerHTML=m+":"+s;}
+    document.getElementById("timer").innerHTML=m+":"+s;
+}
 
 
 var a=true;
-var mm;
-var ss;
+//time 
 function time(){
     if(a){
        mm= setInterval(min, 60000);
@@ -71,6 +126,7 @@ function time(){
         a=true;
     document.getElementById('timer').innerHTML=" ";
     document.getElementById('timer').style.display='none';
+    
     clearInterval(mm);
     clearInterval(ss);
     m=9;
@@ -79,3 +135,33 @@ function time(){
        
 }
 
+
+
+function answer(){
+var all_ul=document.getElementById('sol').querySelectorAll('ul');
+    for (let i = 1; i <= all_ul.length; i++) {
+        let temp = solution['q' + i];
+
+        var temp2 = temp.slice(-1);
+
+        temp2 = Number(temp2);
+        var u_l = all_ul[i - 1];
+        var chi = u_l.querySelectorAll('li');
+        //console.log(chi);
+
+        for (var j = 1; j <= chi.length; j++) {
+            if (temp2 == 0) { break;}
+            if (j == temp2) {
+             
+                chi[j - 1].style.border = '4px solid black';
+            }
+        }
+
+
+    }
+    document.getElementById('sol').style.display='block';
+}
+
+
+
+ 
