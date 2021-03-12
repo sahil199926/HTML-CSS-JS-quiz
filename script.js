9864f0b058d3bba61313f910b19e52f642e092ce
sahil jaryal <60871687+sahil199926@users.noreply.github.com>
2021-03-13 00:17:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
+//start
 function func(){
  document.getElementById('index').style.display='none';
  document.getElementById('q-1').style.display='block';
+ //foot
+ document.getElementById('foot').style.display='block';
 }
-//go privious
-function prev(event){
+//go
+function go(a){
+  
+    var divs=document.getElementById('index');
+    divs=divs.nextElementSibling;
 
-    var a= event.target.parentElement.parentElement.parentElement.parentElement.parentElement;
-    a.style.display='none';
-    a.previousElementSibling.style.display='block';
-    
-}
-//go next
-function next(event){
-
-    var b= event.target.parentElement.parentElement.parentElement.parentElement.parentElement;
-    b.style.display='none';
-    b.nextElementSibling.style.display='block';
-}
-//footer to move qouckly to  any visitted page
-function go(event,a){
-    var b= event.target.parentElement.parentElement.parentElement.parentElement.parentElement;
-    b.style.display='none';
-    var c=document.getElementById('index');
-
-    for(i=0;i<a;i++){
-        c=c.nextElementSibling;
+    for(i=1;i<=10;i++){
+        if(divs.style.display=='block')
+        {var del=document.getElementById('foot').childNodes[1].childNodes[1].querySelectorAll('li');
+        for(j=0;j<del.length;j++){
+            if(i==Number(divs.id.slice(-1)))
+            {
+                del[j].classList.remove('active');
+            }
+            if(j==a){del[j-1].classList.add('active');}
+            
+        }
     }
-    c.style.display='block';
+
+        if(i==a){divs.style.display='block';}
+        else{divs.style.display='none';}
+        divs=divs.nextElementSibling;
+    }
 }
+
+function fo(event){
+
+    var b= event.target.parentElement.parentElement.parentElement;
+
+    var f=document.getElementById('foot').childNodes[1].childNodes[1].querySelectorAll('li');
+    
+    var temp=Number(b.id.slice(-1));
+   
+    f[temp-1].classList.remove("active");
+    f[temp].classList.remove('disabled');
+    f[temp].classList.add('active');
+}
+
+//footer to move quickly to  any visited page
+
 //start again
 function start(){
     document.getElementById('result').style.display='none';
@@ -69,9 +86,15 @@ function result(){
     }
     document.getElementById("res").innerHTML='You have scored '+r;
 }
-
+//mark as submitted
+function tick(event){
+    event.target.innerHTML=" ✓ submitted";
+}
+//modal
 function sure(c){
     if(c=='c'){document.getElementsByClassName('modal')[0].style.display='none';}
+    else if(c=='b'){document.getElementsByClassName('modal')[0].style.display='none';
+    document.getElementById('foot').style.display='none';}
     else{document.getElementsByClassName('modal')[0].style.display='block';}
 
 }
@@ -136,7 +159,7 @@ function time(){
 }
 
 
-
+//amswers
 function answer(){
 var all_ul=document.getElementById('sol').querySelectorAll('ul');
     for (let i = 1; i <= all_ul.length; i++) {
@@ -161,9 +184,28 @@ var all_ul=document.getElementById('sol').querySelectorAll('ul');
     }
     document.getElementById('sol').style.display='block';
 }
-
+//show answers
 function show(){
     document.getElementById('b3').style.display='block';
 }
 
  
+//top
+mybutton = document.getElementById("top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
